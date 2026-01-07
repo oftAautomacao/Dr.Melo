0010728662c2aaa2a1dcabfc3dfd20635c91a487
OftAutomacao <oftautomacao@gmail.com>
2026-01-07 16:22:12 -0300

    07/01/2026 Estatistica

--- src/components/layout/sidebar-layout.tsx
+++ src/components/layout/sidebar-layout.tsx
@@ -2,7 +2,7 @@ import React, { ReactNode } from 'react';
 import Link from 'next/link';
 
 import { Home, CalendarDays, PlusCircle, CalendarX } from "lucide-react"; // Importando ícones
-import { MessageSquareText, Settings } from "lucide-react"; // Importando ícone de mensagem e configurações
+import { MessageSquareText, Settings, BarChart } from "lucide-react"; // Importando ícone de mensagem e configurações
 import WhatsAppIcon from '@/components/ui/whatsapp-icon';
 interface SidebarLayoutProps {
   children: ReactNode;
@@ -35,6 +35,10 @@ const SidebarLayout: React.FC<SidebarLayoutProps> = ({ children, unit, bgColor }
           <Link href="/cancelamentos" className="flex items-center px-4 py-3 rounded transition-colors hover:bg-blue-800">
             <CalendarX className="mr-3 h-5 w-5" /> {/* Ícone */}
             Cancelamentos
+          </Link>
+          <Link href="/estatisticas" className="flex items-center px-4 py-3 rounded transition-colors hover:bg-blue-800">
+            <BarChart className="mr-3 h-5 w-5" /> {/* Ícone */}
+            Estatísticas
           </Link>
           <Link href="/enviar-mensagem" className="flex items-center px-4 py-3 rounded transition-colors hover:bg-blue-800">
             <MessageSquareText className="mr-3 h-5 w-5" /> {/* Ícone de Mensagem */}
